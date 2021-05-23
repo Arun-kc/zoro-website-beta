52fed8812730a12e909e49298ac99e4db945883c
Arun-kc <arunkc91@gmail.com>
2021-05-23 16:40:15 +0530

    Sample home page

---
+++ src/components/pages/HomePage/Data.js
@@ -0,0 +1,54 @@
+export const homeObjOne = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "Unlimited Fun",
+  headline: "Zoro Bot",
+  description: "Fun | Moderation | Utility",
+  buttonLabel: "Invite Me",
+  imgStart: "",
+  img: "images/zoro.png",
+  alt: "Credit card",
+};
+
+export const homeObjTwo = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "100% SECURE",
+  headline: "Endless Fun 🥳",
+  description: `Let your members have some Fun in your server ! \n
+        Many commands with many (or not) utilities`,
+  buttonLabel: "Learn More",
+  imgStart: "",
+  img: "images/zoro.png",
+  alt: "Vault",
+};
+
+export const homeObjThree = {
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: "EASY SETUP",
+  headline: "Super fast and simple onboarding process",
+  description:
+    "Get everything set up and ready in under 10 minutes. All you need to do is add your information and you're ready to go.",
+  buttonLabel: "Start Now",
+  imgStart: "start",
+  img: "images/svg-7.svg",
+  alt: "Vault",
+};
+
+export const homeObjFour = {
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: "DATA ANALYTICS",
+  headline: "Every transaction is stored on our secure cloud database",
+  description:
+    "Never ever have to worry again about saved reciepts. We store your data, so you can access it anytime.",
+  buttonLabel: "Sign Up Now",
+  imgStart: "start",
+  img: "images/svg-8.svg",
+  alt: "Vault",
+};
